fba2f67aa39d6aa9a5e4abb1e9c1cf05f0d1edf8
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2019-06-17 00:25:58 +0100

    fixed the problem with css grid.

--- client/src/components/Timeline.jsx
+++ client/src/components/Timeline.jsx
@@ -2,6 +2,23 @@ import React, { Component } from 'react'
 
 export default class Timeline extends Component {
   render() {
-    return <div className="timelineContainer">Time</div>
+    return (
+      <div className="timelineContainer">
+        <h1>Timeline</h1>
+        <div className="timelineHeaders">
+          <h4>My life</h4>
+          <h4>Career</h4>
+          <small>Important moments of my life and education</small>
+          <small>My path as a Developer</small>
+        </div>
+        <div className="timeLine">
+          <hr width="1" size="1000" />
+          <div className="mylife">
+            <div className="mylifeCard" />
+          </div>
+          <div className="career">career</div>
+        </div>
+      </div>
+    )
   }
 }
